4a570f3be56c872ad8f95dab5f00519bb874382c
Praveen Selvam <praveen.selvam@gmail.com>
2011-11-26 21:35:52 +0530

    Created Program Detail page.

--- header.php
+++ header.php
@@ -3,9 +3,9 @@
     <img src="img/zastra-logo.jpg" />
     <nav>
       <ul>
-        <li class="selected"><a href="javascript: void(0);">Home</a></li>
+        <li><a href="index.php">Home</a></li>
         <li><a href="javascript: void(0);">What is Zastra</a></li>
-        <li><a href="javascript: void(0);">Programs Offered</a></li>
+        <li class="selected"><a href="program.php">Programs Offered</a></li>
         <li><a href="javascript: void(0);">The Team</a></li>
       </ul>
     </nav>
---
+++ program.php
@@ -0,0 +1,85 @@
+<?php include 'page_head.php' ?>
+
+<body>
+  <?php include 'header.php' ?>
+  
+  <section>
+    <div class="container_16">
+      <section class="grid_5" id="list_of_programs">
+        <div class="top_shadow">&nbsp;</div>
+        <ul>
+          <li>
+            <h3>Scholar</h3>
+            <span>Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit.</span>
+          </li>
+          <li>
+            <h3>IT Power</h3>
+            <span>Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit.</span>
+          </li>
+          <li class="selected">
+            <h3>Take Flight</h3>
+            <span>Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit.</span>
+          </li>
+          <li>
+            <h3>Zapper Pro</h3>
+            <span>Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit.</span>
+          </li>
+          <li>
+            <h3>Super Graduate</h3>
+            <span>Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit.</span>
+          </li>
+        </ul>
+        <div class="bottom_shadow">&nbsp;</div>
+      </section>
+      
+      <section class="grid_11" id="program_details">
+        
+        <div class="banner clearfix">
+          <div class="grid_7 content">
+            <h1>Take Flight</h1>
+            <div class="description">
+              A program for non metus quis enim tincidunt consectetur non sed nisi. Vestibulum vitae lobortis quam. Mauris erat nulla, vehicula id mattis iaculis, pretium a est. Phasellus euismod semper ligula, sit amet interdum lacus varius vel.
+            </div>
+            <dl class="clearfix">
+              <dt>Target Audience:</dt>
+              <dd>Pursuing Graduates - 3rd year onwards</dd>
+              <dt>Duration:</dt>
+              <dd>1 year</dd>
+              <dt>Course Fees:</dt>
+              <dd>Rs. 25,000</dd>
+            </dl>
+            <div class="green_button">
+              Enroll Now
+              <div class="end"></div>
+            </div>
+          </div>
+        </div>
+        
+        <div class="more_information">
+          <h4>What is Take Flight?</h4>
+          <div>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus ipsum erat, sollicitudin accumsan mattis sit amet, condimentum venenatis sem. Nulla lacus magna, convallis id laoreet eu, cursus id mauris. Ut rhoncus ipsum sed nisi hendrerit congue. Morbi lobortis odio quis purus interdum ac rhoncus ante ultricies. Cras euismod dolor a.
+            </p>
+          </div>
+          
+          <h4>More about the Take Flight Program</h4>
+          <div>
+            <p>
+              Maecenas bibendum ligula non nunc commodo a ultricies nulla mollis. Ut sit amet sem sapien. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Integer sit amet mi at justo consequat sagittis. Sed leo erat, laoreet at accumsan eget, dictum a mauris. Maecenas nulla magna, consectetur id scelerisque in, lacinia lacinia ipsum. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. In hac habitasse platea dictumst. Praesent tempus tincidunt diam, sit amet pharetra risus fringilla non. Donec eros dui, fringilla vel fringilla a, rhoncus id diam.
+            </p>
+            <p>
+              Vestibulum bibendum elementum lorem id congue. Donec gravida aliquet justo nec tristique. Integer turpis ipsum, volutpat eu tincidunt eu, egestas nec turpis. Phasellus leo nibh, vehicula ut vulputate sed, aliquam sit amet quam. Nulla vulputate risus id dui porta ac iaculis ligula elementum. Nulla vel bibendum risus. Duis placerat facilisis eros, non aliquam tellus viverra eu. Sed eu nulla sed ante eleifend accumsan sit amet eu purus. Pellentesque ut quam et tellus fringilla interdum sed a erat.
+            </p>
+            <p>
+              Curabitur tincidunt semper lacus et cursus. Vivamus ut tempus neque. Suspendisse lacus orci, dapibus ut scelerisque sed, semper at tellus. Nam eget diam metus. Nullam id ipsum non dolor dapibus convallis. Aliquam erat volutpat. Phasellus ultricies felis ut leo porta non tincidunt urna egestas. Vestibulum mattis, nisl at porttitor cursus, lorem magna mattis nulla, nec blandit magna nunc tincidunt est.
+            </p>
+          </div>
+      </section>
+    </div>
+  </section>
+  
+  <?php include 'footer.php' ?>
+  
+</body>
+</html>
